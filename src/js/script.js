f26545d80a64c83256cf673d1e4943fd2885f8dd
GentlemanMenace <kcrundquist@gmail.com>
2020-09-01 22:07:30 -0500

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,7 @@ For assistance:
 */
 const studentList = document.getElementsByClassName('student-list')[0];
 const linkList = document.getElementsByClassName('link-list')[0];
+const header = document.getElementsByClassName('header')[0];
 
 /*
 Create the `showPage` function
@@ -22,6 +23,7 @@ function showPage (list, page) {
    const endIndex = page * 9;
    studentList.innerHTML = '';
 
+   //creates a student list item for each student on the page
    for (let i = 0; i < list.length; i++) {
       if(i >= startIndex && i < endIndex) {
          studentList.insertAdjacentHTML('beforeend', `
@@ -50,6 +52,7 @@ function addPagination(list){
    linkList.innerHTML = '';
 
    for(let i = 1; i <= numOfPages; i++) {
+      //adds active to the first list item
       if(i === 1){
          linkList.insertAdjacentHTML('beforeend', `
             <li>
@@ -64,24 +67,101 @@ function addPagination(list){
       `);
       }
    }
+
+   //This listener occurs on a click event and updates the clicked button with the active class name and displays the next 9 students on the page.
+   linkList.addEventListener ('click', (e) => {
+      if(e.target.tagName === 'BUTTON'){
+         newPage(e.target, list);
+      }
+   });
 }
 
-linkList.addEventListener ('click', (e) => {
-   const button = e.target;
-   const list = data;
+/*
+This function will create and insert/append the elements needed for the search bar
+*/
+function searchBar(){
+   header.insertAdjacentHTML('beforeend', `
+      <label for="search" class="student-search">
+         <input id="search" placeholder="Search by name...">
+         <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+      </label>
+   `);
+}
+
+/*
+This function will create and insert/append the elements needed for the no results element
+*/
+function noResults() {
+   const page = document.getElementsByClassName('page')[0];
+   const div = document.createElement('div');
+   const h1 = document.createElement('h1');
+   h1.className = 'no-results';
+   h1.textContent = 'No results found';
+   div.appendChild(h1);
+   page.insertBefore(div, studentList);
+   div.style.display = 'none';
+}
+
+/*
+This function set the button parameter to active and remove all other button's classes.  It also then calls showPage.
+*/
+function newPage(button, list) {
    const buttonList = linkList.querySelectorAll('button');
 
-   if(e.target.tagName === 'BUTTON'){
-      for(i = 0; i < buttonList.length; i++) {
-         buttonList[i].className = '';
-      }
+   for(i = 0; i < buttonList.length; i++) {
+      buttonList[i].className = '';
+   }
 
-      e.target.className = 'active';
+   button.className = 'active';
 
-      showPage(list, button.textContent);
+   showPage(list, button.textContent);
+}
+
+
+/*
+This function will will return the matched students based on a user's input into a search bar
+*/
+function searchResults(searchInput, list) {
+   const noResults = studentList.previousElementSibling;
+   let matchedStudents = [];
+
+   for(let i = 0; i < list.length; i++) {   
+     if( list[i]['name']['first'].toLowerCase().includes(searchInput.toLowerCase())) {
+       matchedStudents.push(list[i]);
+     } 
+   }
+
+   if (matchedStudents.length === 0) {
+      noResults.style.display = '';
+   } else {
+      noResults.style.display = 'none';
+   }
+
+   return matchedStudents;
+ }
+
+header.addEventListener('click', (e) => {
+   if(e.target.tagName === 'BUTTON') {
+      const input = e.target.previousElementSibling.value;
+      const studentSearchedList = searchResults(input, data);
+
+      showPage(studentSearchedList, 1);
+      addPagination(studentSearchedList);
+   }
+});
+
+header.addEventListener('keyup', (e) => {
+   if(e.target.tagName === 'INPUT') {
+      const input = e.target.value;
+      const studentSearchedList = searchResults(input, data);
+
+      showPage(studentSearchedList, 1);
+      addPagination(studentSearchedList);
    }
 });
 
 // Call functions
+searchBar();
 showPage(data, 1);
 addPagination(data);
+noResults();
